5eb6bfdc10710d1211228c243f14b519d43e9e7f
akcps <ajitkumarjsr15@gmail.com>
2020-09-21 21:11:23 +0530

    Updates

---
+++ precache-manifest.ffdc3ff2fa787be43c47b0bee5e5bc9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221a180439f206b4af9d08ae22212d97",
+    "url": "/react-2048/index.html"
+  },
+  {
+    "revision": "5e19e9362e1eb031e04e",
+    "url": "/react-2048/static/css/main.72a98480.chunk.css"
+  },
+  {
+    "revision": "318247a9b55db27ce487",
+    "url": "/react-2048/static/js/2.1ec578f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-2048/static/js/2.1ec578f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e19e9362e1eb031e04e",
+    "url": "/react-2048/static/js/main.2d897ee1.chunk.js"
+  },
+  {
+    "revision": "dc92f74799cb04b50f62",
+    "url": "/react-2048/static/js/runtime-main.da4e7253.js"
+  }
+]);
